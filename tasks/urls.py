c4e55c1d724e5c3a8c09fcb8c2bf36a2c292092f
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-06 04:08:42 -0500

    eliminar tareas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import create_task, list_tasks
+from .views import create_task, delete_task, list_tasks
 
 urlpatterns = [
     path('', list_tasks),
     path("new_task/", create_task, name="create_task"),
+    path("delete_task/<int:task_id>/", delete_task, name="delete_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,11 @@ def list_tasks(request):
   return render(request, "list_tasks.html", {"tasks": tasks}) 
 
 def create_task(request): 
-  print(request.POST["description"])
   task = Task(title=request.POST["title"], description=request.POST["description"])
   task.save()
+  return redirect("/tasks/")
+
+def delete_task(request, task_id):
+  task = Task.objects.get(id=task_id)
+  task.delete()
   return redirect("/tasks/")
