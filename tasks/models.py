a5971a8230104e9eea69eca21ffbd34165eefb96
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-06 03:12:34 -0500

    Conexión a Postgres

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from turtle import title
+from django.db import models
+
+# Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
