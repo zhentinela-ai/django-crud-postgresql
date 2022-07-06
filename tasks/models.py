477ab3606f5af2949bce881006e6fd27aa785a51
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-06 03:27:00 -0500

    Vista de Tareas

--- tasks/models.py
+++ tasks/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+  title = models.CharField(max_length=100)
+  description = models.TextField(blank=True)
