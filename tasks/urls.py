5b876c1456743583cfafe0ead3ac2c6628b80e65
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-07-06 03:54:05 -0500

    crear tareas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import list_tasks
+from .views import create_task, list_tasks
 
 urlpatterns = [
     path('', list_tasks),
+    path("new_task/", create_task, name="create_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def list_tasks(request): 
   return render(request, "list_tasks.html")
+
+def create_task(request): 
+  print(request.POST["description"])
+  task = Task(title=request.POST["title"], description=request.POST["description"])
+  task.save()
+  return redirect("/tasks/")
